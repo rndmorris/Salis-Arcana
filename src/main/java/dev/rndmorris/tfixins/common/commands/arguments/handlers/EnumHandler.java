4dbea64d60c2931b3198b78af2c2fab8f25b6a7a
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-11-26 03:23:12 -0600

    Unify enum handling

---
+++ src/main/java/dev/rndmorris/tfixins/common/commands/arguments/handlers/EnumHandler.java
@@ -0,0 +1,50 @@
+package dev.rndmorris.tfixins.common.commands.arguments.handlers;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+
+import dev.rndmorris.tfixins.lib.EnumHelper;
+
+public abstract class EnumHandler<E extends Enum<E>> implements IArgumentHandler {
+
+    protected final Class<E> enumDefinition;
+
+    public EnumHandler(Class<E> enumDefinition) {
+        this.enumDefinition = enumDefinition;
+    }
+
+    @Override
+    public Object parse(ICommandSender sender, String current, Iterator<String> args) {
+        E result = null;
+        String name = null;
+
+        if (args.hasNext()) {
+            name = args.next();
+            result = EnumHelper.tryParseEnum(enumDefinition.getEnumConstants(), name);
+        }
+
+        if (result == null) {
+            throw new CommandException("tfixins:error.unknown_value", name);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<String> getAutocompleteOptions(ICommandSender sender, String current, Iterator<String> args) {
+        if (args.hasNext()) {
+            args.next();
+            if (!args.hasNext()) {
+                return Arrays.stream(enumDefinition.getEnumConstants())
+                    .map(E::toString)
+                    .collect(Collectors.toList());
+            }
+        }
+        return null;
+    }
+}
