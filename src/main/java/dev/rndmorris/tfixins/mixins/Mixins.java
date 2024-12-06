7893db04a6d0deb57a646796c620cc3c92857dd4
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-12-06 13:50:41 -0600

    Remove dependency on ordinal()

--- src/main/java/dev/rndmorris/tfixins/mixins/Mixins.java
+++ src/main/java/dev/rndmorris/tfixins/mixins/Mixins.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import cpw.mods.fml.relauncher.FMLLaunchHandler;
+import cpw.mods.fml.relauncher.Side;
 import dev.rndmorris.tfixins.config.FixinsConfig;
 import dev.rndmorris.tfixins.config.IEnabler;
 
@@ -13,33 +14,33 @@ public enum Mixins {
     // spotless:off
 
     // Biome colors
-    BIOME_EERIE_MODULE(Side.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenEerie"),
-    BIOME_ELDRITCH_MODULE(Side.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenEldritch"),
-    BIOME_MAGICALFOREST_MODULE(Side.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenMagicalForest"),
-    BIOME_TAINT_MODULE(Side.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenTaint"),
+    BIOME_EERIE_MODULE(MixinSide.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenEerie"),
+    BIOME_ELDRITCH_MODULE(MixinSide.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenEldritch"),
+    BIOME_MAGICALFOREST_MODULE(MixinSide.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenMagicalForest"),
+    BIOME_TAINT_MODULE(MixinSide.BOTH, FixinsConfig.biomeColorModule, "biomes.MixinBiomeGenTaint"),
 
     // Bugfixes
-    ARCANE_FURNACE_DUPE_FIX(Side.BOTH, FixinsConfig.bugfixesModule.infernalFurnaceDupeFix, "blocks.MixinBlockArcaneFurnace"),
-    BEACON_BLOCKS(Side.BOTH, FixinsConfig.bugfixesModule.blockCosmeticSolidBeaconFix, "blocks.MixinBlockCosmeticSolid"),
-    BLOCKCANDLE_OOB(Side.BOTH, FixinsConfig.bugfixesModule.candleRendererCrashes, "blocks.MixinBlockCandleRenderer", "blocks.MixinBlockCandle"),
-    DEAD_MOBS_DONT_ATTACK(Side.BOTH, FixinsConfig.bugfixesModule.deadMobsDontAttack, "entities.MixinEntityTaintacle", "entities.MixinEntityEldritchCrab", "entities.MixinEntityThaumicSlime"),
-    ITEMSHARD_OOB(Side.BOTH, FixinsConfig.bugfixesModule.itemShardColor, "items.MixinItemShard"),
-    RENDER_REDSTONE_FIX(Side.BOTH, FixinsConfig.bugfixesModule.renderRedstoneFix, "blocks.MixinBlockCustomOre", "blocks.MixinBlockMetalDevice", "blocks.MixinBlockStoneDevice", "blocks.MixinBlockWoodenDevice", "blocks.MixinBlockTaint"),
+    ARCANE_FURNACE_DUPE_FIX(MixinSide.BOTH, FixinsConfig.bugfixesModule.infernalFurnaceDupeFix, "blocks.MixinBlockArcaneFurnace"),
+    BEACON_BLOCKS(MixinSide.BOTH, FixinsConfig.bugfixesModule.blockCosmeticSolidBeaconFix, "blocks.MixinBlockCosmeticSolid"),
+    BLOCKCANDLE_OOB(MixinSide.BOTH, FixinsConfig.bugfixesModule.candleRendererCrashes, "blocks.MixinBlockCandleRenderer", "blocks.MixinBlockCandle"),
+    DEAD_MOBS_DONT_ATTACK(MixinSide.BOTH, FixinsConfig.bugfixesModule.deadMobsDontAttack, "entities.MixinEntityTaintacle", "entities.MixinEntityEldritchCrab", "entities.MixinEntityThaumicSlime"),
+    ITEMSHARD_OOB(MixinSide.BOTH, FixinsConfig.bugfixesModule.itemShardColor, "items.MixinItemShard"),
+    RENDER_REDSTONE_FIX(MixinSide.BOTH, FixinsConfig.bugfixesModule.renderRedstoneFix, "blocks.MixinBlockCustomOre", "blocks.MixinBlockMetalDevice", "blocks.MixinBlockStoneDevice", "blocks.MixinBlockWoodenDevice", "blocks.MixinBlockTaint"),
 
     // Baubles
-    EXTENDED_BAUBLES_SUPPORT(Side.BOTH, FixinsConfig.bugfixesModule.useAllBaublesSlots, "events.MixinEventHandlerRunic", "items.MixinWandManager"),
-    EXTENDED_BAUBLES_SUPPORT_CLIENT(Side.CLIENT, FixinsConfig.bugfixesModule.useAllBaublesSlots, "gui.MixinREHWandHandler"),
+    EXTENDED_BAUBLES_SUPPORT(MixinSide.BOTH, FixinsConfig.bugfixesModule.useAllBaublesSlots, "events.MixinEventHandlerRunic", "items.MixinWandManager"),
+    EXTENDED_BAUBLES_SUPPORT_CLIENT(MixinSide.CLIENT, FixinsConfig.bugfixesModule.useAllBaublesSlots, "gui.MixinREHWandHandler"),
 
     // Thaumonomicon
-    CTRL_SCROLL_NAVIGATION(Side.CLIENT, FixinsConfig.researchBrowserModule.scrollwheelEnabled, "gui.MixinGuiResearchBrowser"),
-    RESEARCH_ID_POPUP(Side.CLIENT, FixinsConfig.researchBrowserModule.showResearchId, "gui.MixinGuiResearchBrowser"),
-    RIGHT_CLICK_NAVIAGTION(Side.CLIENT, FixinsConfig.researchBrowserModule.rightClickClose, "gui.MixinGuiResearchBrowser", "gui.MixinGuiResearchRecipe"),
+    CTRL_SCROLL_NAVIGATION(MixinSide.CLIENT, FixinsConfig.researchBrowserModule.scrollwheelEnabled, "gui.MixinGuiResearchBrowser"),
+    RESEARCH_ID_POPUP(MixinSide.CLIENT, FixinsConfig.researchBrowserModule.showResearchId, "gui.MixinGuiResearchBrowser"),
+    RIGHT_CLICK_NAVIAGTION(MixinSide.CLIENT, FixinsConfig.researchBrowserModule.rightClickClose, "gui.MixinGuiResearchBrowser", "gui.MixinGuiResearchRecipe"),
 
     ;
     // spotless:on
 
     private final List<String> classes;
-    private final Side side;
+    private final MixinSide side;
     private final IEnabler config;
 
     public static List<String> getMixins() {
@@ -52,21 +53,28 @@ public enum Mixins {
         return mixins;
     }
 
-    Mixins(Side side, IEnabler config, String... classes) {
+    Mixins(MixinSide side, IEnabler config, String... classes) {
         this.side = side;
         this.config = config;
         this.classes = Arrays.asList(classes);
     }
 
     private boolean isEnabled() {
-        boolean loadSide = this.side.ordinal() == 2 || this.side.ordinal() == FMLLaunchHandler.side()
-            .ordinal();
-        return loadSide && this.config.isEnabled();
+        return side.matchesFMLSide(FMLLaunchHandler.side()) && this.config.isEnabled();
     }
 
-    private enum Side {
+    private enum MixinSide {
+
         CLIENT,
         SERVER,
-        BOTH
+        BOTH;
+
+        public boolean matchesFMLSide(Side side) {
+            return switch (this) {
+                case BOTH -> true;
+                case CLIENT -> side == Side.CLIENT;
+                case SERVER -> side == Side.SERVER;
+            };
+        }
     }
 }
