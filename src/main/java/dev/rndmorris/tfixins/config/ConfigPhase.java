08d9ec2713513e19f0a6715e9f2f00cbb16d1f51
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-12-16 17:06:52 -0600

    docstrings

--- src/main/java/dev/rndmorris/tfixins/config/ConfigPhase.java
+++ src/main/java/dev/rndmorris/tfixins/config/ConfigPhase.java
@@ -1,6 +1,6 @@
 package dev.rndmorris.tfixins.config;
 
 public enum ConfigPhase {
-    EARLY,
-    LATE
+    EARLY, // Will be loaded before mixins are loaded
+    LATE // Will be loaded during preinit
 }
