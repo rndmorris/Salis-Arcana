1f76fdd90cd5d6647e992d98beedf11731580747
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-12-16 13:51:47 -0600

    Register config to suppress creative warp

--- src/main/java/dev/rndmorris/tfixins/config/bugfixes/BugfixesModule.java
+++ src/main/java/dev/rndmorris/tfixins/config/bugfixes/BugfixesModule.java
@@ -7,6 +7,7 @@ import javax.annotation.Nonnull;
 import net.minecraftforge.common.config.Configuration;
 
 import dev.rndmorris.tfixins.config.IConfigModule;
+import dev.rndmorris.tfixins.config.Setting;
 import dev.rndmorris.tfixins.config.ToggleSetting;
 
 public class BugfixesModule implements IConfigModule {
@@ -19,32 +20,42 @@ public class BugfixesModule implements IConfigModule {
     public final ToggleSetting infernalFurnaceDupeFix;
     public final ToggleSetting itemShardColor;
     public final ToggleSetting renderRedstoneFix;
+    public final ToggleSetting suppressWarpEventsInCreative;
     public final ToggleSetting useAllBaublesSlots;
+
+    private final Setting[] settings;
 
     public BugfixesModule() {
         final var thisRef = new WeakReference<IConfigModule>(this);
-        blockCosmeticSolidBeaconFix = new BlockCosmeticSolidBeaconFix(thisRef);
-        candleRendererCrashes = new ToggleSetting(
-            thisRef,
-            "candleMetadataCrash",
-            "Fixes several crashes with invalid candle metadata");
-        deadMobsDontAttack = new ToggleSetting(
-            thisRef,
-            "deadMobsDontAttack",
-            "Prevents eldritch crabs, all taintacles, and thaumic slimes from attacking during their death animation.");
-        infernalFurnaceDupeFix = new ToggleSetting(
-            thisRef,
-            "infernalFurnaceDupeFix",
-            "Fixes a smelting duplication glitch with the Infernal Furnace");
-        itemShardColor = new ToggleSetting(thisRef, "shardMetadataCrash", "Fixes a crash with invalid shard metadata");
-        renderRedstoneFix = new ToggleSetting(
-            thisRef,
-            "renderRedstoneFix",
-            "Fixes an issue with ores where they don't get rendered as normal blocks, not allowing you to push a redstone signal through them.");
-        useAllBaublesSlots = new ToggleSetting(
-            thisRef,
-            "useAllBaublesSlots",
-            "Enables support for mods that increase the number of baubles slots.");
+        settings = new Setting[] { blockCosmeticSolidBeaconFix = new BlockCosmeticSolidBeaconFix(thisRef),
+            candleRendererCrashes = new ToggleSetting(
+                thisRef,
+                "candleMetadataCrash",
+                "Fixes several crashes with invalid candle metadata"),
+            deadMobsDontAttack = new ToggleSetting(
+                thisRef,
+                "deadMobsDontAttack",
+                "Prevents eldritch crabs, all taintacles, and thaumic slimes from attacking during their death animation."),
+            infernalFurnaceDupeFix = new ToggleSetting(
+                thisRef,
+                "infernalFurnaceDupeFix",
+                "Fixes a smelting duplication glitch with the Infernal Furnace"),
+            itemShardColor = new ToggleSetting(
+                thisRef,
+                "shardMetadataCrash",
+                "Fixes a crash with invalid shard metadata"),
+            renderRedstoneFix = new ToggleSetting(
+                thisRef,
+                "renderRedstoneFix",
+                "Fixes an issue with ores where they don't get rendered as normal blocks, not allowing you to push a redstone signal through them."),
+            suppressWarpEventsInCreative = new ToggleSetting(
+                thisRef,
+                "suppressWarpEventsInCreative",
+                "Prevent random warp events from firing for players in creative mode."),
+            useAllBaublesSlots = new ToggleSetting(
+                thisRef,
+                "useAllBaublesSlots",
+                "Enables support for mods that increase the number of baubles slots."), };
     }
 
     @Nonnull
@@ -66,13 +77,9 @@ public class BugfixesModule implements IConfigModule {
 
     @Override
     public void loadModuleFromConfig(@Nonnull Configuration configuration) {
-        blockCosmeticSolidBeaconFix.loadFromConfiguration(configuration);
-        candleRendererCrashes.loadFromConfiguration(configuration);
-        deadMobsDontAttack.loadFromConfiguration(configuration);
-        itemShardColor.loadFromConfiguration(configuration);
-        useAllBaublesSlots.loadFromConfiguration(configuration);
-        renderRedstoneFix.loadFromConfiguration(configuration);
-        infernalFurnaceDupeFix.loadFromConfiguration(configuration);
+        for (var setting : settings) {
+            setting.loadFromConfiguration(configuration);
+        }
     }
 
     @Override
