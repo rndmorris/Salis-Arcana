490e3012cbd0f6af74a786be1a9e92fd4cce8788
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-01-10 14:56:58 -0600

    dont change phial stack on insert or extract

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemEssence.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemEssence.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,13 +17,18 @@ public class MixinItemEssence {
 
     @Inject(
         method = "onItemUseFirst",
-        at = @At(value = "INVOKE", target = "Lthaumcraft/common/tiles/TileJarFillable;markDirty()V"),
+        at = @At(
+            value = "FIELD",
+            opcode = Opcodes.PUTFIELD,
+            target = "Lnet/minecraft/item/ItemStack;stackSize:I",
+            shift = At.Shift.AFTER),
         cancellable = true)
     private void onItemUseFirst(ItemStack itemstack, EntityPlayer player, World world, int x, int y, int z, int side,
         float f1, float f2, float f3, CallbackInfoReturnable<Boolean> cir) {
         if (player.capabilities.isCreativeMode) {
+            itemstack.stackSize += 1;
             world.playSoundAtEntity(player, "game.neutral.swim", 0.25F, 1.0F);
-            player.inventoryContainer.detectAndSendChanges();
+            // player.inventoryContainer.detectAndSendChanges();
             cir.setReturnValue(true);
             cir.cancel();
         }
