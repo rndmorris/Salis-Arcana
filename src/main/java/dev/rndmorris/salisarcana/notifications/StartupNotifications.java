7b0712ddbfb6b1d09c67155a2065b54930ca5402
Midnight145 <23142346+Midnight145@users.noreply.github.com>
2025-01-19 22:04:34 -0600

    Send a notification at startup if tcinventoryscan is also installed (#104)
    * notice if tcinventoryscan is independently installed
    
    * method for each notice instead of all in one method

--- src/main/java/dev/rndmorris/salisarcana/notifications/StartupNotifications.java
+++ src/main/java/dev/rndmorris/salisarcana/notifications/StartupNotifications.java
@@ -2,23 +2,37 @@ package dev.rndmorris.salisarcana.notifications;
 
 import net.minecraft.util.ChatComponentTranslation;
 
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
 
 public class StartupNotifications {
 
-    private boolean noticeSent = false;
+    private boolean deprecationNoticeSent = false;
+
+    private boolean tcInventoryScanNoticeSent = !Loader.isModLoaded("tcinventoryscan");
 
     @SubscribeEvent
     public void onLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        sendDeprecationNotice(event);
+        sendTCInventoryScanNotice(event);
+    }
+
+    private void sendDeprecationNotice(PlayerEvent.PlayerLoggedInEvent event) {
         final var biomeColors = ConfigModuleRoot.biomeColors;
-        if (noticeSent || !biomeColors.isEnabled() || biomeColors.acknowledgeDeprecation.isEnabled()) {
-            return;
+        if (!deprecationNoticeSent && biomeColors.isEnabled() && !biomeColors.acknowledgeDeprecation.isEnabled()) {
+            deprecationNoticeSent = true;
+            event.player.addChatMessage(new ChatComponentTranslation("salisarcana:biome_color_deprecation"));
+            event.player.addChatMessage(new ChatComponentTranslation("salisarcana:biome_color_deprecation_suppress"));
         }
-        noticeSent = true;
-        event.player.addChatMessage(new ChatComponentTranslation("salisarcana:biome_color_deprecation"));
-        event.player.addChatMessage(new ChatComponentTranslation("salisarcana:biome_color_deprecation_suppress"));
+    }
+
+    private void sendTCInventoryScanNotice(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!tcInventoryScanNoticeSent) {
+            tcInventoryScanNoticeSent = true;
+            event.player.addChatMessage(new ChatComponentTranslation("salisarcana:tcinventoryscan_notice"));
+        }
     }
 
 }
