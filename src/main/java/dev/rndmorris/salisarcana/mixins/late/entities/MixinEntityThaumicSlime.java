3ed9acac73933548bb04517e1e020a4d3963bb38
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-01-23 23:45:02 -0600

    Replace injects and redirects with wraps in MixinEntityThaumicSlime

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/entities/MixinEntityThaumicSlime.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/entities/MixinEntityThaumicSlime.java
@@ -7,37 +7,39 @@ import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import thaumcraft.common.entities.monster.EntityThaumicSlime;
 
 @Mixin(value = EntityThaumicSlime.class)
-public class MixinEntityThaumicSlime extends EntityMob {
+public abstract class MixinEntityThaumicSlime extends EntityMob {
 
     public MixinEntityThaumicSlime(World p_i1738_1_) {
         super(p_i1738_1_);
     }
 
-    @Inject(method = "onCollideWithPlayer", at = @At("HEAD"), remap = false, cancellable = true)
-    public void onOnCollideWithPlayer(EntityPlayer player, CallbackInfo ci) {
+    @WrapMethod(method = "onCollideWithPlayer")
+    public void onOnCollideWithPlayer(EntityPlayer player, Operation<Void> original) {
         if (!this.isEntityAlive()) {
-            ci.cancel();
+            return;
         }
+        original.call(player);
     }
 
-    @Redirect(
+    @WrapOperation(
         method = "updateEntityActionState",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/World;spawnEntityInWorld(Lnet/minecraft/entity/Entity;)Z",
             ordinal = 0))
-    public boolean proxySpawnEntityInWorld(World target, Entity entity) {
+    public boolean proxySpawnEntityInWorld(World target, Entity entity, Operation<Boolean> original) {
         if (!this.isEntityAlive()) {
             return false;
         }
-        return worldObj.spawnEntityInWorld(entity);
+        return original.call(target, entity);
     }
 
 }
