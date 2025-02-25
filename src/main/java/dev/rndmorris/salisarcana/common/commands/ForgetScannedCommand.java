e25646852aa86afc631f3620af607d86ae20aea6
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-02-24 18:40:19 -0600

    Add --aspects to forget-scanned

--- src/main/java/dev/rndmorris/salisarcana/common/commands/ForgetScannedCommand.java
+++ src/main/java/dev/rndmorris/salisarcana/common/commands/ForgetScannedCommand.java
@@ -21,8 +21,10 @@ import dev.rndmorris.salisarcana.common.commands.arguments.annotations.FlagArg;
 import dev.rndmorris.salisarcana.common.commands.arguments.annotations.NamedArg;
 import dev.rndmorris.salisarcana.common.commands.arguments.handlers.IArgumentHandler;
 import dev.rndmorris.salisarcana.common.commands.arguments.handlers.flag.FlagHandler;
+import dev.rndmorris.salisarcana.common.commands.arguments.handlers.named.AspectHandler;
 import dev.rndmorris.salisarcana.common.commands.arguments.handlers.named.PlayerHandler;
 import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
+import thaumcraft.api.aspects.Aspect;
 import thaumcraft.common.Thaumcraft;
 import thaumcraft.common.lib.research.ScanManager;
 import thaumcraft.common.lib.utils.BlockUtils;
@@ -38,7 +40,7 @@ public class ForgetScannedCommand extends ArcanaCommandBase<ForgetScannedCommand
         return new ArgumentProcessor<>(
             Arguments.class,
             Arguments::new,
-            new IArgumentHandler[] { PlayerHandler.INSTANCE, FlagHandler.INSTANCE, });
+            new IArgumentHandler[] { PlayerHandler.INSTANCE, FlagHandler.INSTANCE, AspectHandler.INSTANCE, });
     }
 
     @Override
@@ -90,6 +92,20 @@ public class ForgetScannedCommand extends ArcanaCommandBase<ForgetScannedCommand
                 }
             }
         }
+
+        if (arguments.aspects != null && !arguments.aspects.isEmpty()) {
+            final var playerAspects = playerKnowledge.aspectsDiscovered
+                .get(arguments.targetPlayer.getCommandSenderName());
+            if (playerAspects != null) {
+                for (var aspect : arguments.aspects) {
+                    if (playerAspects.aspects.containsKey(aspect)) {
+                        removedCount++;
+                        playerAspects.aspects.remove(aspect);
+                    }
+                }
+            }
+        }
+
         if (arguments.all || arguments.objects) {
             toClear.add(playerKnowledge.objectsScanned);
         }
@@ -165,6 +181,9 @@ public class ForgetScannedCommand extends ArcanaCommandBase<ForgetScannedCommand
 
         @FlagArg(name = "--all", excludes = { "--objects", "--entities", "--nodes" }, descLangKey = "all")
         public boolean all;
+
+        @NamedArg(name = "--aspects", handler = AspectHandler.class, descLangKey = "aspect")
+        public ArrayList<Aspect> aspects;
 
         @FlagArg(name = "--hand", excludes = "--all", descLangKey = "hand")
         public boolean hand;
