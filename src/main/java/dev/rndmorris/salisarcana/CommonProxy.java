ccc0dde3873e8285cdd5dda6b76ec11ca472b385
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-03-21 15:19:06 -0500

    move forget aspect functionality into new command

--- src/main/java/dev/rndmorris/salisarcana/CommonProxy.java
+++ src/main/java/dev/rndmorris/salisarcana/CommonProxy.java
@@ -17,6 +17,7 @@ import dev.rndmorris.salisarcana.common.CustomResearch;
 import dev.rndmorris.salisarcana.common.blocks.CustomBlocks;
 import dev.rndmorris.salisarcana.common.commands.ArcanaCommandBase;
 import dev.rndmorris.salisarcana.common.commands.CreateNodeCommand;
+import dev.rndmorris.salisarcana.common.commands.ForgetAspectCommand;
 import dev.rndmorris.salisarcana.common.commands.ForgetResearchCommand;
 import dev.rndmorris.salisarcana.common.commands.ForgetScannedCommand;
 import dev.rndmorris.salisarcana.common.commands.HelpCommand;
@@ -84,6 +85,7 @@ public class CommonProxy {
         maybeRegister(event, commands.createNode, CreateNodeCommand::new);
         maybeRegister(event, commands.forgetResearch, ForgetResearchCommand::new);
         maybeRegister(event, commands.forgetScanned, ForgetScannedCommand::new);
+        maybeRegister(event, commands.forgetAspects, ForgetAspectCommand::new);
         maybeRegister(event, commands.help, HelpCommand::new);
         maybeRegister(event, commands.infusionSymmetry, InfusionSymmetryCommand::new);
         maybeRegister(event, commands.prerequisites, PrerequisitesCommand::new);
