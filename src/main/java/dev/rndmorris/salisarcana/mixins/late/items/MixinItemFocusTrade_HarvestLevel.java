8ce318e27f0da6867d9670b50bd071b130426741
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-03-29 16:57:47 -0500

    equal trade focus respects harvest level

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusTrade_HarvestLevel.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusTrade_HarvestLevel.java
@@ -6,35 +6,39 @@ import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
+import thaumcraft.api.IArchitect;
+import thaumcraft.api.wands.ItemFocusBasic;
 import thaumcraft.common.items.wands.foci.ItemFocusTrade;
 
 @Mixin(value = ItemFocusTrade.class, remap = false)
-public class MixinItemFocusTrade_HarvestLevel {
+public abstract class MixinItemFocusTrade_HarvestLevel extends ItemFocusBasic implements IArchitect {
 
-    @Inject(
+    @Shadow
+    abstract protected MovingObjectPosition getMovingObjectPositionFromPlayer(World par1World,
+        EntityPlayer par2EntityPlayer);
+
+    @WrapOperation(
         method = "onFocusRightClick",
         at = @At(
-            value = "INVOKE_ASSIGN",
-            target = "Lthaumcraft/common/items/wands/foci/ItemFocusTrade;getPickedBlock(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"),
-        locals = LocalCapture.PRINT)
-    private void wrapGetPickedBlock(ItemStack itemstack, World world, EntityPlayer player,
-        MovingObjectPosition movingobjectposition, CallbackInfoReturnable<ItemStack> cir,
-        @Local(ordinal = 5) MovingObjectPosition mop) {
+            value = "INVOKE",
+            target = "Lthaumcraft/common/items/wands/foci/ItemFocusTrade;getPickedBlock(Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"))
+    private ItemStack wrapGetPickedBlock(ItemFocusTrade instance, ItemStack stack, Operation<ItemStack> original,
+        @Local(name = "world") World world, @Local(name = "player") EntityPlayer player) {
+        MovingObjectPosition mop = this.getMovingObjectPositionFromPlayer(world, player);
         final int x = mop.blockX, y = mop.blockY, z = mop.blockZ;
         final var block = world.getBlock(x, y, z);
         final var metadata = world.getBlockMetadata(x, y, z);
-        if (block.getHarvestLevel(metadata) < ConfigModuleRoot.enhancements.equalTradeFocusHarvestLevel.getValue()) {
-            cir.setReturnValue(itemstack);
-            cir.cancel();
+        if (block.getHarvestLevel(metadata) <= ConfigModuleRoot.enhancements.equalTradeFocusHarvestLevel.getValue()) {
+            return original.call(instance, stack);
         }
+        return null;
     }
-
 }
