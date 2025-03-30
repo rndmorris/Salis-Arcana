b4192c7fa18701e9d52d0ef670abd32eda4e2714
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-03-29 21:05:40 -0500

    block will try and fail to break, and play thaumcraft:craftfail instead of silently doing nothing

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusExcavation_HarvestLevel.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusExcavation_HarvestLevel.java
@@ -3,7 +3,6 @@ package dev.rndmorris.salisarcana.mixins.late.items;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,28 +11,21 @@ import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
-import thaumcraft.api.wands.FocusUpgradeType;
 import thaumcraft.api.wands.ItemFocusBasic;
 import thaumcraft.common.items.wands.foci.ItemFocusExcavation;
 
 @Mixin(ItemFocusExcavation.class)
 public class MixinItemFocusExcavation_HarvestLevel extends ItemFocusBasic {
 
-    @WrapMethod(method = "onFocusRightClick", remap = false)
-    public ItemStack wrapOnFocusRightClick(ItemStack itemstack, World world, EntityPlayer p, MovingObjectPosition mop,
-        Operation<ItemStack> original) {
-        if (mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
-            Block block = p.worldObj.getBlock(mop.blockX, mop.blockY, mop.blockZ);
-            int metadata = p.worldObj.getBlockMetadata(mop.blockX, mop.blockY, mop.blockZ);
-            int requiredLevel = block.getHarvestLevel(metadata);
-            int harvestLevel = ConfigModuleRoot.enhancements.excavationFocusHarvestLevel.getValue();
-            if (ConfigModuleRoot.enhancements.potencyModifiesHarvestLevel.isEnabled()) {
-                harvestLevel += this.getUpgradeLevel(itemstack, FocusUpgradeType.potency);
-            }
-            if (harvestLevel < 0 || harvestLevel >= requiredLevel) {
-                return original.call(itemstack, world, p, mop);
-            }
+    @WrapMethod(method = "excavate", remap = false)
+    private boolean wrapExcavate(World world, ItemStack stack, EntityPlayer player, Block block, int md, int x, int y,
+        int z, Operation<Boolean> original) {
+        int requiredLevel = block.getHarvestLevel(md);
+        int harvestLevel = ConfigModuleRoot.enhancements.excavationFocusHarvestLevel.getValue();
+        if (harvestLevel < 0 || harvestLevel >= requiredLevel) {
+            return original.call(world, stack, player, block, md, x, y, z);
         }
-        return itemstack;
+        player.worldObj.playSoundEffect(player.posX, player.posY, player.posZ, "thaumcraft:craftfail", 1.0F, 1.0F);
+        return false;
     }
 }
