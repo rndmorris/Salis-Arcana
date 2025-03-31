51876c6d19a29886b34f75a26523bde0c7e493f7
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-03-31 18:55:46 -0500

    botania-style thaumonomicon browsing + bugfix with rclick navigation, closes #209

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/gui/MixinGuiResearchBrowser_RightClickClose.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/gui/MixinGuiResearchBrowser_RightClickClose.java
@@ -1,22 +1,50 @@
 package dev.rndmorris.salisarcana.mixins.late.gui;
 
+import static dev.rndmorris.salisarcana.lib.MixinHelpers.RightClickClose$ScreenStack;
+
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.Tuple;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Shadow;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+
+import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
+import thaumcraft.api.research.ResearchItem;
 import thaumcraft.client.gui.GuiResearchBrowser;
+import thaumcraft.client.gui.GuiResearchRecipe;
 
 @Mixin(value = GuiResearchBrowser.class)
 public class MixinGuiResearchBrowser_RightClickClose extends GuiScreen {
 
-    @Inject(method = "mouseClicked", at = @At(value = "HEAD"), cancellable = true)
-    private void onMouseClicked(int mouseX, int mouseY, int button, CallbackInfo ci) {
+    @Shadow(remap = false)
+    protected double guiMapX;
+
+    @Shadow(remap = false)
+    protected double guiMapY;
+
+    @WrapMethod(method = "initGui")
+    public void wrapInitGui(Operation<Void> original) {
+        original.call();
+        if (!RightClickClose$ScreenStack.isEmpty() && ConfigModuleRoot.enhancements.nomiconSavePage.isEnabled()) {
+            Tuple currentPage = RightClickClose$ScreenStack.pop();
+            this.mc.displayGuiScreen(
+                new GuiResearchRecipe(
+                    (ResearchItem) currentPage.getFirst(),
+                    (int) currentPage.getSecond(),
+                    this.guiMapX,
+                    this.guiMapY));
+        }
+    }
+
+    @WrapMethod(method = "mouseClicked")
+    private void onMouseClicked(int mouseX, int mouseY, int button, Operation<Void> operation) {
         if (button == 1) {
             this.mc.displayGuiScreen(null);
-            ci.cancel();
+            return;
         }
+        operation.call(mouseX, mouseY, button);
     }
 }
--- src/main/java/dev/rndmorris/salisarcana/mixins/late/gui/MixinGuiResearchRecipe.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/gui/MixinGuiResearchRecipe.java
@@ -1,6 +1,6 @@
 package dev.rndmorris.salisarcana.mixins.late.gui;
 
-import java.util.Stack;
+import static dev.rndmorris.salisarcana.lib.MixinHelpers.RightClickClose$ScreenStack;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
@@ -11,9 +11,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import dev.rndmorris.salisarcana.config.ConfigModuleRoot;
 import thaumcraft.api.research.ResearchItem;
 import thaumcraft.client.gui.GuiResearchBrowser;
 import thaumcraft.client.gui.GuiResearchRecipe;
@@ -23,31 +26,40 @@ public class MixinGuiResearchRecipe extends GuiScreen {
 
     @Shadow(remap = false)
     protected double guiMapX;
+
     @Shadow(remap = false)
     protected double guiMapY;
+
     @Shadow(remap = false)
     private int page;
+
     @Shadow(remap = false)
     private ResearchItem research;
 
+    // Switching pages doesn't init a new GuiResearchRecipe, so that's not automatically added to the stack in initGui.
+    // We have to track whenever a page changes manually
     @Unique
-    private static final Stack<Tuple> sa$screenStack = new Stack<>();
+    private int sa$currentPage = 0;
 
-    @Inject(method = "<init>", at = @At(value = "TAIL"), remap = false)
-    private void onInit(ResearchItem research, int page, double x, double y, CallbackInfo ci) {
-        sa$screenStack.push(new Tuple(research, page));
+    @WrapMethod(method = "initGui")
+    private void onInit(Operation<Void> original) {
+        RightClickClose$ScreenStack.push(new Tuple(research, page));
+        this.sa$currentPage = page;
+        original.call();
     }
 
-    @Inject(method = "mouseClicked", at = @At(value = "HEAD"), cancellable = true)
-    private void onMouseClicked(int mouseX, int mouseY, int button, CallbackInfo ci) {
+    @WrapMethod(method = "mouseClicked")
+    private void wrapMouseClicked(int mouseX, int mouseY, int button, Operation<Void> original) {
         if (button == 1) {
-            if (sa$screenStack.size() == 1) {
-                sa$screenStack.clear();
+            if ((RightClickClose$ScreenStack.size() <= 1)) {
+                // it can be zero in the case of opening directly to a page, where the page was originally the only
+                // thing in the stack
+                RightClickClose$ScreenStack.clear();
                 this.mc.displayGuiScreen(new GuiResearchBrowser());
             } else {
                 EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-                sa$screenStack.pop(); // current screen
-                Tuple item = sa$screenStack.pop();
+                RightClickClose$ScreenStack.pop(); // current screen
+                Tuple item = RightClickClose$ScreenStack.pop(); // next screen
                 player.worldObj.playSound(player.posX, player.posY, player.posZ, "thaumcraft:page", 0.66F, 1.0F, false);
                 this.mc.displayGuiScreen(
                     new GuiResearchRecipe(
@@ -56,7 +68,30 @@ public class MixinGuiResearchRecipe extends GuiScreen {
                         this.guiMapX,
                         this.guiMapY));
             }
-            ci.cancel();
+            return;
         }
+
+        original.call(mouseX, mouseY, button);
+
+        // mouseClicked is the only place where the page can change (someone clicked the arrows)
+        // so we check against the current page and push it to the stack if necessary.
+        if (this.sa$currentPage != this.page) {
+            this.sa$currentPage = this.page;
+            RightClickClose$ScreenStack.push(new Tuple(this.research, this.page));
+        }
+    }
+
+    @WrapOperation(
+        method = "keyTyped",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V"))
+    private void onKeyTyped(Minecraft instance, GuiScreen i, Operation<Void> original) {
+        if (ConfigModuleRoot.enhancements.nomiconSavePage.isEnabled()) {
+            // just close the window entirely
+            original.call(instance, null);
+            return;
+        }
+        original.call(instance, i);
     }
 }
