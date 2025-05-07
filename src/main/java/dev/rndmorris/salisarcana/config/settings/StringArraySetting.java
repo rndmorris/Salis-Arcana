030c161101db688d19f3a146042d858b81d493d1
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-05-07 15:50:30 -0500

    override isenabled

--- src/main/java/dev/rndmorris/salisarcana/config/settings/StringArraySetting.java
+++ src/main/java/dev/rndmorris/salisarcana/config/settings/StringArraySetting.java
@@ -32,4 +32,9 @@ public class StringArraySetting extends Setting {
     public String[] getValue() {
         return value;
     }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled() && value != defaultValue;
+    }
 }
