6be627f64a8f0c6ee09546f78848450f4e76d04e
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-05-07 23:07:15 -0500

    remove accidental entities var in StringArraySetting

--- src/main/java/dev/rndmorris/salisarcana/config/settings/StringArraySetting.java
+++ src/main/java/dev/rndmorris/salisarcana/config/settings/StringArraySetting.java
@@ -1,8 +1,5 @@
 package dev.rndmorris.salisarcana.config.settings;
 
-import java.util.HashSet;
-
-import net.minecraft.entity.Entity;
 import net.minecraftforge.common.config.Configuration;
 
 import dev.rndmorris.salisarcana.config.IEnabler;
@@ -13,8 +10,6 @@ public class StringArraySetting extends Setting {
     protected final String comment;
     protected final String[] defaultValue;
     protected String[] value;
-
-    protected final HashSet<Class<? extends Entity>> entities = new HashSet<>();
 
     public StringArraySetting(IEnabler dependency, String name, String comment, String[] defaultValue) {
         super(dependency);
