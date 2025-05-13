a8ab9fb4860d3aed5fd9a492c7b5fad1ca2fcfac
nicksitnikov <nicksitnikov@gmail.com>
2025-05-13 03:34:43 -0500

    Nearly everything works, except the Obelisk Renderer. It might be impossible, actually...

--- src/main/java/dev/rndmorris/salisarcana/client/EldritchBlockItemRenderer.java
+++ src/main/java/dev/rndmorris/salisarcana/client/EldritchBlockItemRenderer.java
@@ -1,11 +1,16 @@
 package dev.rndmorris.salisarcana.client;
 
+import dev.rndmorris.salisarcana.lib.R;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.client.IItemRenderer;
 import org.lwjgl.opengl.GL11;
 import thaumcraft.client.renderers.block.BlockRenderer;
+import thaumcraft.common.blocks.BlockCosmeticSolid;
 import thaumcraft.common.blocks.BlockEldritch;
 import thaumcraft.common.config.ConfigBlocks;
 import thaumcraft.common.tiles.TileEldritchAltar;
@@ -18,6 +23,10 @@ public class EldritchBlockItemRenderer implements IItemRenderer {
     private final TileEldritchCap capstoneTile = new TileEldritchCap();
     private final TileEldritchCrabSpawner crustedOpeningTile = new TileEldritchCrabSpawner();
     private final TileEldritchObelisk obeliskTile = new TileEldritchObelisk();
+
+    public EldritchBlockItemRenderer() {
+        R.of(crustedOpeningTile).set("facing", (byte) 5);
+    }
 
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
@@ -34,43 +43,55 @@ public class EldritchBlockItemRenderer implements IItemRenderer {
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
         RenderBlocks renderer = (RenderBlocks) data[0];
 
-        switch (item.getItemDamage()) {
-            case 0:
-                renderTileSimple(altarTile);
-                break;
-            case 1:
-                renderObelisk();
-                break;
-            case 3:
-                renderTileSimple(capstoneTile);
-                break;
-            case 8:
-                renderLockingMechanism(type, renderer);
-                break;
-            case 9:
-                renderTileSimple(crustedOpeningTile);
-                break;
-        }
-    }
-
-    private void renderTileSimple(TileEntity tileEntity) {
-
-    }
-
-    private void renderObelisk() {
-
-    }
-
-    private void renderLockingMechanism(ItemRenderType type, RenderBlocks renderer) {
-        final var icons = ((BlockEldritch) ConfigBlocks.blockEldritch).insIcon;
-
         GL11.glPushMatrix();
         if(type == ItemRenderType.EQUIPPED || type == ItemRenderType.EQUIPPED_FIRST_PERSON) {
             GL11.glTranslatef(0.5f, 0.5f, 0.5f);
         }
 
-        renderer.setRenderBounds(0f, 0f, 0f, 1f, 1f, 1f);
-        BlockRenderer.drawFaces(renderer, ConfigBlocks.blockEldritch, icons[4], icons[4], icons[4], icons[3], icons[4], icons[4], true);
+        switch (item.getItemDamage()) {
+            case 0:
+                renderTileSimple(altarTile, 0D);
+                break;
+            case 1:
+                renderObelisk();
+                break;
+            case 3:
+                renderTileSimple(capstoneTile, 0D);
+                break;
+            case 8:
+                final var icons = ((BlockEldritch) ConfigBlocks.blockEldritch).insIcon;
+                renderer.setRenderBounds(0f, 0f, 0f, 1f, 1f, 1f);
+                BlockRenderer.drawFaces(renderer, ConfigBlocks.blockEldritch, icons[4], icons[4], icons[4], icons[3], icons[4], icons[4], true);
+                break;
+            case 9:
+                final var icon = ((BlockCosmeticSolid) ConfigBlocks.blockCosmeticSolid).icon[25];
+                renderer.setRenderBounds(0f, 0f, 0f, 1f, 1f, 1f);
+                BlockRenderer.drawFaces(renderer, ConfigBlocks.blockEldritch, icon, true);
+
+                // For some reason, the tile entity doesn't match up with the block on its own.
+                renderTileSimple(crustedOpeningTile, -0.1D);
+                break;
+        }
+
         GL11.glPopMatrix();
+    }
+
+    private void renderTileSimple(TileEntity tileEntity, double y) {
+        TileEntityRendererDispatcher.instance.renderTileEntityAt(tileEntity, 0D, y, 0D, 0F);
+    }
+
+    private void renderObelisk() {
+        final var viewpoint = Minecraft.getMinecraft().renderViewEntity;
+
+        // Prevent far-away rendering optimizations from triggering.
+        obeliskTile.xCoord = MathHelper.floor_double(viewpoint.posX);
+        obeliskTile.yCoord = MathHelper.floor_double(viewpoint.posY);
+        obeliskTile.zCoord = MathHelper.floor_double(viewpoint.posZ);
+
+        GL11.glScalef(0.25f, 0.25f, 0.25f);
+
+        // TODO This doesn't work!!! The tile renderer requires the player's position relative to the obelisk!
+
+        renderTileSimple(obeliskTile, -2D);
     }
 }
--- src/main/java/dev/rndmorris/salisarcana/mixins/late/client/renderers/MixinBlockEldritchRenderer.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/client/renderers/MixinBlockEldritchRenderer.java
@@ -1,12 +1,11 @@
 package dev.rndmorris.salisarcana.mixins.late.client.renderers;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.util.IIcon;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thaumcraft.client.renderers.block.BlockEldritchRenderer;
 import thaumcraft.client.renderers.block.BlockRenderer;
 import thaumcraft.common.blocks.BlockEldritch;
@@ -14,8 +13,8 @@ import thaumcraft.common.config.ConfigBlocks;
 
 @Mixin(BlockEldritchRenderer.class)
 public class MixinBlockEldritchRenderer {
-    @Inject(method = "renderInventoryBlock", at = @At("HEAD"), remap = false)
-    public void renderSolidBlocks(Block block, int metadata, int modelID, RenderBlocks renderer, CallbackInfo ci) {
+    @WrapMethod(method = "renderInventoryBlock", remap = false)
+    public void renderSolidBlocks(Block block, int metadata, int modelID, RenderBlocks renderer, Operation<Void> original) {
         final IIcon[] icons = ((BlockEldritch) ConfigBlocks.blockEldritch).insIcon;
 
         if(metadata == 7) {
@@ -24,6 +23,8 @@ public class MixinBlockEldritchRenderer {
         } else if (metadata == 10) {
             renderer.setRenderBounds(0f, 0f, 0f, 1f, 1f, 1f);
             BlockRenderer.drawFaces(renderer, block, icons[5], icons[7], icons[6], icons[5], icons[6], icons[8], true);
+        } else {
+            original.call(block, metadata, modelID, renderer);
         }
     }
 }
