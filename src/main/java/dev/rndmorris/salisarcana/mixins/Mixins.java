1670741d4569a3e7d5d68e51c5f04467c9e095be
nicksitnikov <nicksitnikov@gmail.com>
2025-06-04 22:07:10 -0500

    Modify the phial spending behavior of Banners

--- src/main/java/dev/rndmorris/salisarcana/mixins/Mixins.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/Mixins.java
@@ -151,6 +151,11 @@ public enum Mixins {
         .setApplyIf(SalisConfig.bugfixes.runedStoneIgnoreCreative::isEnabled)
         .addMixinClasses("tiles.MixinTileEldritchTrap_CreativeImmunity")
         .addTargetedMod(TargetedMod.THAUMCRAFT)),
+    BANNER_PHIAL_CONSUMPTION(new Builder().setPhase(Phase.LATE)
+        .setSide(Side.BOTH)
+        .setApplyIf(SalisConfig.bugfixes.bannerPhialConsumption::applyMixin)
+        .addMixinClasses("blocks.MixinBlockWoodenDevice_BannerPhialConsumption")
+        .addTargetedMod(TargetedMod.THAUMCRAFT)),
 
     // Features
     EXTENDED_BAUBLES_SUPPORT(new Builder().setPhase(Phase.LATE)
