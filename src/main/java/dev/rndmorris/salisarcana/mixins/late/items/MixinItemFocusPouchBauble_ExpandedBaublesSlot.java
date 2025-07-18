507476a561c71a90fcebe7007ddbfb1d9223e324
nicksitnikov <nicksitnikov@gmail.com>
2025-07-17 20:51:49 -0500

    Setup mixins

---
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusPouchBauble_ExpandedBaublesSlot.java
@@ -0,0 +1,20 @@
+package dev.rndmorris.salisarcana.mixins.late.items;
+
+import baubles.api.BaubleType;
+import baubles.api.expanded.IBaubleExpanded;
+import dev.rndmorris.salisarcana.common.compat.BaublesExpandedCompat;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import thaumcraft.common.items.wands.ItemFocusPouchBauble;
+
+@Mixin(ItemFocusPouchBauble.class)
+public abstract class MixinItemFocusPouchBauble_ExpandedBaublesSlot implements IBaubleExpanded {
+    @Shadow
+    public abstract BaubleType getBaubleType(ItemStack itemstack);
+
+    @Override
+    public String[] getBaubleTypes(ItemStack itemStack) {
+        return new String[] { BaublesExpandedCompat.POUCH_SLOT };
+    }
+}
