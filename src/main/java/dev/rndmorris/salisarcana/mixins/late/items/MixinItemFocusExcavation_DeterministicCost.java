f765c45194c0327523afd8dca5b5de0bf63a45ef
nicksitnikov <nicksitnikov@gmail.com>
2025-07-21 15:14:10 -0500

    Replace star import with single-class imports.

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusExcavation_DeterministicCost.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemFocusExcavation_DeterministicCost.java
@@ -2,7 +2,11 @@ package dev.rndmorris.salisarcana.mixins.late.items;
 
 import net.minecraft.item.ItemStack;
 
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
