b2a6ba2143a89e88e80f783ae2b60b5192f87772
Midnight145 <23142346+Midnight145@users.noreply.github.com>
2025-07-29 18:39:43 -0500

    Correctly sync aspect updates to the client when `/forget-aspect` is used in multi-player (#305)
    **What kind of change does this PR introduce?** (Bug fix, feature, docs
    update, ...)
    bugfix
    
    **What is the current behavior?** (You can also link to an open issue
    here)
    #271 
    
    **What is the new behavior (if this is a feature change)?**
    closes #271
    
    **Does this PR introduce a breaking change?**
    no
    
    **Other information**:
    draft pr because untested, should work though
    
    ---------
    
    Co-authored-by: Midnight145 <23142346+Midnight145@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/dev/rndmorris/salisarcana/network/NetworkHandler.java
+++ src/main/java/dev/rndmorris/salisarcana/network/NetworkHandler.java
@@ -11,12 +11,13 @@ public class NetworkHandler {
     public static void init() {
         // long message type names make spotless weird
         // spotless:off
-        instance.registerMessage(MessageScanContainer.class, MessageScanContainer.class, 2, Side.SERVER);
-        instance.registerMessage(MessageScanIInventory.class, MessageScanIInventory.class, 3, Side.SERVER);
-        instance.registerMessage(MessageDuplicateResearch.class, MessageDuplicateResearch.class, 4, Side.SERVER);
-        instance.registerMessage(MessageForgetResearch.class, MessageForgetResearch.class, 6, Side.CLIENT);
-        instance.registerMessage(MessageForgetScannedObjects.class, MessageForgetScannedObjects.class, 7, Side.CLIENT);
-        instance.registerMessage(MessageForgetScannedCategory.class, MessageForgetScannedCategory.class, 8, Side.CLIENT);
+        instance.registerMessage(MessageScanContainer.class, MessageScanContainer.class, 1, Side.SERVER);
+        instance.registerMessage(MessageScanIInventory.class, MessageScanIInventory.class, 2, Side.SERVER);
+        instance.registerMessage(MessageDuplicateResearch.class, MessageDuplicateResearch.class, 3, Side.SERVER);
+        instance.registerMessage(MessageForgetResearch.class, MessageForgetResearch.class, 4, Side.CLIENT);
+        instance.registerMessage(MessageForgetAspects.class, MessageForgetAspects.class, 5, Side.CLIENT);
+        instance.registerMessage(MessageForgetScannedObjects.class, MessageForgetScannedObjects.class, 6, Side.CLIENT);
+        instance.registerMessage(MessageForgetScannedCategory.class, MessageForgetScannedCategory.class, 7, Side.CLIENT);
         // spotless:on
     }
 }
