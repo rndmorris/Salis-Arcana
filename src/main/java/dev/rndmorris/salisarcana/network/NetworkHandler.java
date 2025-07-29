15f59a66e161289a63e8b248def72f4387370955
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-07-28 21:42:53 -0500

    [Bugfix] `forget-scanned` now updates the client in multi-player (#327)
    **What kind of change does this PR introduce?** (Bug fix, feature, docs
    update, ...)
    Bug fix
    
    **What is the current behavior?** (You can also link to an open issue
    here)
    #318 Updates to a player's scanned lists require a re-log to sync to the
    client.
    
    **What is the new behavior (if this is a feature change)?**
    Updates to a player's scanned lists are synced to the affected client
    via network packet.
    
    **Does this PR introduce a breaking change?**
    No
    
    **Other information**:
    N/A

--- src/main/java/dev/rndmorris/salisarcana/network/NetworkHandler.java
+++ src/main/java/dev/rndmorris/salisarcana/network/NetworkHandler.java
@@ -9,9 +9,14 @@ public class NetworkHandler {
     public static final SimpleNetworkWrapper instance = new SimpleNetworkWrapper(SalisArcana.MODID);
 
     public static void init() {
+        // long message type names make spotless weird
+        // spotless:off
         instance.registerMessage(MessageScanContainer.class, MessageScanContainer.class, 2, Side.SERVER);
         instance.registerMessage(MessageScanIInventory.class, MessageScanIInventory.class, 3, Side.SERVER);
         instance.registerMessage(MessageDuplicateResearch.class, MessageDuplicateResearch.class, 4, Side.SERVER);
         instance.registerMessage(MessageForgetResearch.class, MessageForgetResearch.class, 6, Side.CLIENT);
+        instance.registerMessage(MessageForgetScannedObjects.class, MessageForgetScannedObjects.class, 7, Side.CLIENT);
+        instance.registerMessage(MessageForgetScannedCategory.class, MessageForgetScannedCategory.class, 8, Side.CLIENT);
+        // spotless:on
     }
 }
