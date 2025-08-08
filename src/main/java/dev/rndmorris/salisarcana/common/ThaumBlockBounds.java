979e4bc65691b3b9e574691af3f8c2b997e599f3
nicksitnikov <nicksitnikov@gmail.com>
2025-08-07 19:14:26 -0500

    Add potential block bound solution - is inheritance a problem?

---
+++ src/main/java/dev/rndmorris/salisarcana/common/ThaumBlockBounds.java
@@ -0,0 +1,28 @@
+package dev.rndmorris.salisarcana.common;
+
+import thaumcraft.client.renderers.block.BlockRenderer;
+import thaumcraft.common.config.ConfigBlocks;
+
+public class ThaumBlockBounds {
+    public static void setCorrectBlockBounds() {
+        ConfigBlocks.blockLootUrn.setBlockBounds(
+            BlockRenderer.W2,
+            BlockRenderer.W1,
+            BlockRenderer.W2,
+            BlockRenderer.W14,
+            BlockRenderer.W13,
+            BlockRenderer.W14);
+
+        ConfigBlocks.blockLootCrate.setBlockBounds(
+            BlockRenderer.W1,
+            0.0F,
+            BlockRenderer.W1,
+            BlockRenderer.W15,
+            BlockRenderer.W14,
+            BlockRenderer.W15);
+
+        ConfigBlocks.blockEssentiaReservoir.setBlockBounds(
+            BlockRenderer.W2, BlockRenderer.W2, BlockRenderer.W2, BlockRenderer.W14, BlockRenderer.W14, BlockRenderer.W14
+        );
+    }
+}
