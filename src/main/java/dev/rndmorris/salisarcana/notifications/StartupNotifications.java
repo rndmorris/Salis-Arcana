224cbc68bdf778e79117566352a08931022305aa
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-08-12 14:30:26 -0500

    startup notifications framework for config warnings, etc.

--- src/main/java/dev/rndmorris/salisarcana/notifications/StartupNotifications.java
+++ src/main/java/dev/rndmorris/salisarcana/notifications/StartupNotifications.java
@@ -1,6 +1,9 @@
 package dev.rndmorris.salisarcana.notifications;
 
+import java.util.ArrayList;
+
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
 
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -8,18 +11,29 @@ import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class StartupNotifications {
 
-    private boolean tcInventoryScanNoticeSent = !Loader.isModLoaded("tcinventoryscan");
+    private static final ArrayList<IChatComponent> queue = new ArrayList<>();
 
-    @SubscribeEvent
-    public void onLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        sendTCInventoryScanNotice(event);
-    }
-
-    private void sendTCInventoryScanNotice(PlayerEvent.PlayerLoggedInEvent event) {
-        if (!tcInventoryScanNoticeSent) {
-            tcInventoryScanNoticeSent = true;
-            event.player.addChatMessage(new ChatComponentTranslation("salisarcana:tcinventoryscan_notice"));
+    static {
+        if (Loader.isModLoaded("tcinventoryscan")) {
+            queue.add(new ChatComponentTranslation("salisarcana:tcinventoryscan_notice"));
         }
     }
 
+    @SubscribeEvent
+    public void onLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        for (IChatComponent message : queue) {
+            event.player.addChatMessage(message);
+        }
+        queue.clear();
+    }
+
+    /**
+     * Queues a one-time message to be sent to players when they log in.
+     * Should be used for startup notifications for things like mod compatibility, config errors, etc.
+     *
+     * @param message The message to queue.
+     */
+    public static void queueMessage(IChatComponent message) {
+        queue.add(message);
+    }
 }
