66420803cf518d34e10b0e89dd08bc6b69cba49d
nicksitnikov <nicksitnikov@gmail.com>
2025-08-13 15:19:57 -0500

    Allow wands to get reset in a crafting grid

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemWandCasting_DefaultWandComponents.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/items/MixinItemWandCasting_DefaultWandComponents.java
@@ -1,8 +1,16 @@
 package dev.rndmorris.salisarcana.mixins.late.items;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -13,7 +21,13 @@ import thaumcraft.api.wands.WandRod;
 import thaumcraft.common.items.wands.ItemWandCasting;
 
 @Mixin(value = ItemWandCasting.class, remap = false)
-public class MixinItemWandCasting_DefaultWandComponents {
+public abstract class MixinItemWandCasting_DefaultWandComponents {
+
+    @Shadow
+    public abstract WandCap getCap(ItemStack stack);
+
+    @Shadow
+    public abstract WandRod getRod(ItemStack stack);
 
     @WrapMethod(method = "getCap")
     public WandCap defaultCap(ItemStack stack, Operation<WandCap> original) {
@@ -34,6 +48,16 @@ public class MixinItemWandCasting_DefaultWandComponents {
             } else {
                 return WandHelper.ROD_UNKNOWN;
             }
+        }
+    }
+
+    @Inject(method = "addInformation", at = @At("HEAD"), remap = true)
+    private void addInvalidPartMessage(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced,
+        CallbackInfo ci) {
+        final var cap = this.getCap(stack);
+        final var rod = this.getRod(stack);
+        if (cap == WandHelper.CAP_UNKNOWN || rod == WandHelper.ROD_UNKNOWN || rod == WandHelper.STAFF_UNKNOWN) {
+            tooltip.add(StatCollector.translateToLocal("salisarcana:unknown_wand.craft"));
         }
     }
 }
