667ff63a3b3b3ffaebeb30a5df1e43638ebff2c5
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-08-21 18:51:13 -0500

    Define pick-related oredict names

--- src/main/java/dev/rndmorris/salisarcana/api/OreDict.java
+++ src/main/java/dev/rndmorris/salisarcana/api/OreDict.java
@@ -11,4 +11,18 @@ public class OreDict {
     public static final String HEAT_SOURCE = "salisarcana:heatSource";
     public static final String GREATWOOD_PLANKS = "plankGreatwood";
     public static final String SILVERWOOD_PLANKS = "plankSilverwood";
+
+    /**
+     * Blocks with this oredict tag will always be included in the output of the Pickaxe of the Core's right-click
+     * ability.
+     */
+    // this intentionally contains the term "ore" so that it will be accepted as an "ore" oredict name for TC's ore
+    // scanning
+    public static final String ELEMENTAL_PICK_SCAN_INCLUDE = "salisarcana:pickaxeCoreScanInclude";
+
+    /**
+     * Blocks with this oredict tag will not be included in the output of the Pickaxe of the Core's right-click ability,
+     * even if they normally would be.
+     */
+    public static final String ELEMENTAL_PICK_SCAN_EXCLUDE = "salisarcana:pickaxeCoreScanExclude";
 }
