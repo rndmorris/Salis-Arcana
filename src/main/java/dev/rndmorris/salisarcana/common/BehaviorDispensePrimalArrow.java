30ec20f0bd7d16353a5ae5baf838d64089e4df82
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-08-24 13:19:46 -0500

    Add explanatory comment

--- src/main/java/dev/rndmorris/salisarcana/common/BehaviorDispensePrimalArrow.java
+++ src/main/java/dev/rndmorris/salisarcana/common/BehaviorDispensePrimalArrow.java
@@ -14,9 +14,12 @@ public class BehaviorDispensePrimalArrow implements IBehaviorDispenseItem {
 
     @Override
     public ItemStack dispense(IBlockSource source, ItemStack stack) {
+        // This is implemented similarly to how potions are dispensed
         return new BehaviorProjectileDispense() {
 
             @Override
+            // Required to access both the exact item being dispensed and
+            // the arrow entity without relying on state
             protected IProjectile getProjectileEntity(World worldIn, IPosition position) {
                 final var entity = new EntityPrimalArrow(worldIn, position.getX(), position.getY(), position.getZ());
                 entity.type = stack.getItemDamage() % 6;
