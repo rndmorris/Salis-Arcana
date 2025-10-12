7d43aeb64015dfe146acc64d8d48b387f29c39a0
rndmorris <13648552+rndmorris@users.noreply.github.com>
2025-10-12 17:30:13 -0500

    Replace static const with local const

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/tiles/MixinTileNode_DynamicReach_Pure.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/tiles/MixinTileNode_DynamicReach_Pure.java
@@ -2,7 +2,6 @@ package dev.rndmorris.salisarcana.mixins.late.tiles;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,9 +19,6 @@ import thaumcraft.common.tiles.TileNode;
 @Mixin(value = TileNode.class, remap = false)
 public class MixinTileNode_DynamicReach_Pure extends TileThaumcraft {
 
-    @Unique
-    private static final int sa$NODE_LOG_METADATA = 2;
-
     @Shadow
     AspectList aspects;
 
@@ -36,7 +32,8 @@ public class MixinTileNode_DynamicReach_Pure extends TileThaumcraft {
     private void calculateSizeMultiplier(boolean change, CallbackInfoReturnable<Boolean> cir,
         @Share("sizeMultiplier") LocalDoubleRef sizeMultiplierRef) {
         final var visSize = this.aspects.visSize();
-        if (this.blockType == ConfigBlocks.blockMagicalLog && this.blockMetadata == sa$NODE_LOG_METADATA
+        final var nodeLogMetadata = 2;
+        if (this.blockType == ConfigBlocks.blockMagicalLog && this.blockMetadata == nodeLogMetadata
             && visSize <= DynamicNodeLogic.NODE_SIZE_SMALL_MAX) {
             // special exception for sufficiently-small in-log nodes
             sizeMultiplierRef.set(DynamicNodeLogic.calculateSmallSizeMultiplier(visSize));
