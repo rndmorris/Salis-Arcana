6bfbf113893bf6685a2e94da9071c14ee97abe61
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-11-29 07:38:36 +0100

    Alex/asm cleanup (#409)
    **What kind of change does this PR introduce?** (Bug fix, feature, docs
    update, ...)
    - Changes IAsmEditor interface to transform the whole classnode
    
    ---------
    
    Co-authored-by: rndmorris <13648552+rndmorris@users.noreply.github.com>
    Co-authored-by: Nikolay S. <nicksitnikov@gmail.com>
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/dev/rndmorris/salisarcana/core/SalisArcanaCore.java
+++ src/main/java/dev/rndmorris/salisarcana/core/SalisArcanaCore.java
@@ -1,6 +1,5 @@
 package dev.rndmorris.salisarcana.core;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,14 +7,12 @@ import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import dev.rndmorris.salisarcana.config.SalisConfig;
-import dev.rndmorris.salisarcana.core.asm.IAsmEditor;
-import dev.rndmorris.salisarcana.core.asm.compat.ModCompatEditor;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
+@IFMLLoadingPlugin.TransformerExclusions("dev.rndmorris.salisarcana.core")
 public class SalisArcanaCore implements IFMLLoadingPlugin {
 
-    public static boolean isObfuscated;
-
+    private static Boolean isObf;
     public static final String MODID = "salisarcana";
     public static final Logger LOG = LogManager.getLogger("salisarcana-core");
 
@@ -23,11 +20,9 @@ public class SalisArcanaCore implements IFMLLoadingPlugin {
         SalisConfig.synchronizeConfiguration();
     }
 
-    public static ArrayList<IAsmEditor> editors = new ArrayList<>();
-
     @Override
     public String[] getASMTransformerClass() {
-        return new String[] { "dev.rndmorris.salisarcana.core.SalisArcanaClassTransformer" };
+        return new String[] { "dev.rndmorris.salisarcana.core.asm.SalisArcanaClassTransformer" };
     }
 
     @Override
@@ -42,24 +37,18 @@ public class SalisArcanaCore implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data) {
-        isObfuscated = (boolean) data.get("runtimeDeobfuscationEnabled");
-
-        editors.add(
-            new ModCompatEditor(
-                "xyz.uniblood.thaumicmixins.mixinplugin.ThaumicMixinsLateMixins",
-                "getMixins",
-                "(Ljava/util/Set;)Ljava/util/List;")
-                    .addConflict("MixinBlockCosmeticSolid", SalisConfig.bugfixes.beaconBlockFixSetting)
-                    .addConflict("MixinBlockCandleRenderer", SalisConfig.bugfixes.candleRendererCrashes)
-                    .addConflict("MixinBlockCandle", SalisConfig.bugfixes.candleRendererCrashes)
-                    .addConflict("MixinItemShard", SalisConfig.bugfixes.itemShardColor)
-                    .addConflict("MixinWandManager", SalisConfig.features.useAllBaublesSlots)
-                    .addConflict("MixinEventHandlerRunic", SalisConfig.features.useAllBaublesSlots)
-                    .addConflict("MixinWarpEvents_BaubleSlots", SalisConfig.features.useAllBaublesSlots));
+        isObf = (boolean) data.get("runtimeDeobfuscationEnabled");
     }
 
     @Override
     public String getAccessTransformerClass() {
         return null;
+    }
+
+    public static boolean isObf() {
+        if (isObf == null) {
+            throw new IllegalStateException("Obfuscation state has been accessed too early!");
+        }
+        return isObf;
     }
 }
