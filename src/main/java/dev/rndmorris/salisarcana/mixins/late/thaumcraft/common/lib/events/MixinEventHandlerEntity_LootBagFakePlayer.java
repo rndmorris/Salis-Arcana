4e4fa69e5930f5e4616af8e78f35ef6dd37c9240
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-12-30 05:38:44 -0500

    Simplify fake-player-lootbag mixin

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/lib/events/MixinEventHandlerEntity_LootBagFakePlayer.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/lib/events/MixinEventHandlerEntity_LootBagFakePlayer.java
@@ -1,29 +1,15 @@
 package dev.rndmorris.salisarcana.mixins.late.thaumcraft.common.lib.events;
 
-import net.minecraftforge.common.util.FakePlayer;
-import net.minecraftforge.event.entity.living.LivingDropsEvent;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import thaumcraft.common.lib.events.EventHandlerEntity;
 
 @Mixin(value = EventHandlerEntity.class, remap = false, priority = 1001)
 public abstract class MixinEventHandlerEntity_LootBagFakePlayer {
 
-    @Definition(id = "FakePlayer", type = FakePlayer.class, remap = true)
-    @Definition(id = "event", local = @Local(name = "event", type = LivingDropsEvent.class))
-    @Definition(
-        id = "source",
-        field = "Lnet/minecraftforge/event/entity/living/LivingDropsEvent;source:Lnet/minecraft/util/DamageSource;")
-    @Definition(id = "getEntity", method = "Lnet/minecraft/util/DamageSource;getEntity()Lnet/minecraft/entity/Entity;")
-    @Expression("event.source.getEntity() instanceof FakePlayer")
-    @ModifyExpressionValue(method = "livingDrops", at = @At("MIXINEXTRAS:EXPRESSION"))
+    @ModifyVariable(method = "livingDrops", at = @At("STORE"), name = "fakeplayer")
     private static boolean fakePlayersDropLootBags(boolean original) {
         return false;
     }
