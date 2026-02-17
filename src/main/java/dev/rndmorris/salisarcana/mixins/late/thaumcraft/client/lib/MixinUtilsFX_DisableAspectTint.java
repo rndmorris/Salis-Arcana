547b6af22aa6f6daa6dd7c4381d8ea0461fbf89f
Darren Eberly <darren.eberly@proton.me>
2026-02-17 14:48:58 -0500

    spotless

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/client/lib/MixinUtilsFX_DisableAspectTint.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/client/lib/MixinUtilsFX_DisableAspectTint.java
@@ -3,6 +3,7 @@ package dev.rndmorris.salisarcana.mixins.late.thaumcraft.client.lib;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.client.lib.UtilsFX;
 
@@ -10,15 +11,10 @@ import thaumcraft.client.lib.UtilsFX;
 public class MixinUtilsFX_DisableAspectTint {
 
     @Redirect(
-        method="drawTag(DDLthaumcraft/api/aspects/Aspect;FIDIFZ)V",
-        at = @At(
-            value = "INVOKE",
-            target="Lthaumcraft/api/aspects/Aspect;getColor()I"
-        )
-    )
+        method = "drawTag(DDLthaumcraft/api/aspects/Aspect;FIDIFZ)V",
+        at = @At(value = "INVOKE", target = "Lthaumcraft/api/aspects/Aspect;getColor()I"))
     private static int salisarcana$disableAspectTint(Aspect instance) {
         return 16777215;
     }
-
 
 }
