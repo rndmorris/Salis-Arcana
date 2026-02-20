2e25ac0c4bc428668fb493eff29adda9cfdb23fb
Darren Eberly <darren.eberly@proton.me>
2026-02-20 14:19:08 -0500

    Make mixin abstract and use WrapOperation instead of Redirect

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/client/lib/MixinUtilsFX_DisableAspectTint.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/client/lib/MixinUtilsFX_DisableAspectTint.java
@@ -2,18 +2,20 @@ package dev.rndmorris.salisarcana.mixins.late.thaumcraft.client.lib;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.client.lib.UtilsFX;
 
 @Mixin(value = UtilsFX.class, remap = false)
-public class MixinUtilsFX_DisableAspectTint {
+public abstract class MixinUtilsFX_DisableAspectTint {
 
-    @Redirect(
+    @WrapOperation(
         method = "drawTag(DDLthaumcraft/api/aspects/Aspect;FIDIFZ)V",
         at = @At(value = "INVOKE", target = "Lthaumcraft/api/aspects/Aspect;getColor()I"))
-    private static int salisarcana$disableAspectTint(Aspect instance) {
+    private static int salisarcana$disableAspectTint(Aspect instance, Operation<Integer> original) {
         // Return white such that the tint is effectively disabled.
         return 0xFFFFFF;
     }
