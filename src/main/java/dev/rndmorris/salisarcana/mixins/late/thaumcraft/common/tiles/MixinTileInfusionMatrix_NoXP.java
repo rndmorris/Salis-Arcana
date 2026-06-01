c5663873abd070ee8104b5e5016c293514722de5
Fabian Maurer <dark.shadow4@web.de>
2026-06-01 20:30:49 +0200

    Use better mixin injectors

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/tiles/MixinTileInfusionMatrix_NoXP.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/tiles/MixinTileInfusionMatrix_NoXP.java
@@ -2,30 +2,33 @@ package dev.rndmorris.salisarcana.mixins.late.thaumcraft.common.tiles;
 
 import net.minecraft.entity.player.EntityPlayer;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
 
 import thaumcraft.common.tiles.TileInfusionMatrix;
 
 @Mixin(TileInfusionMatrix.class)
 public class MixinTileInfusionMatrix_NoXP {
 
-    @WrapOperation(
+    @ModifyExpressionValue(
         method = "craftCycle",
-        at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/EntityPlayer;experienceLevel:I"))
-    private int creativeXP(EntityPlayer player, Operation<Integer> original) {
-        return player.capabilities.isCreativeMode ? Integer.MAX_VALUE : original.call(player);
+        at = @At(
+            value = "FIELD",
+            opcode = Opcodes.GETFIELD,
+            target = "Lnet/minecraft/entity/player/EntityPlayer;experienceLevel:I"))
+    private int creativeXP(int original, @Local EntityPlayer player) {
+        return player.capabilities.isCreativeMode ? Integer.MAX_VALUE : original;
     }
 
-    @WrapOperation(
+    @WrapWithCondition(
         method = "craftCycle",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/EntityPlayer;addExperienceLevel(I)V"))
-    private void skipCreativeXPDrain(EntityPlayer player, int levels, Operation<Void> original) {
-        if (!player.capabilities.isCreativeMode) {
-            original.call(player, levels);
-        }
+    private boolean skipCreativeXPDrain(EntityPlayer player, int levels) {
+        return !player.capabilities.isCreativeMode;
     }
 }
