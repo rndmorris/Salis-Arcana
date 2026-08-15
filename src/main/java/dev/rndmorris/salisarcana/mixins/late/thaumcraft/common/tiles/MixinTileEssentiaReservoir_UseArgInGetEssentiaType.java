408722b84c7607942fc968e11986d87659cc4777
rndmorris <13648552+rndmorris@users.noreply.github.com>
2026-08-14 19:02:35 -0500

    Add missing opcode, remove @Local

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/tiles/MixinTileEssentiaReservoir_UseArgInGetEssentiaType.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/common/tiles/MixinTileEssentiaReservoir_UseArgInGetEssentiaType.java
@@ -2,11 +2,11 @@ package dev.rndmorris.salisarcana.mixins.late.thaumcraft.common.tiles;
 
 import net.minecraftforge.common.util.ForgeDirection;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 
 import thaumcraft.common.tiles.TileEssentiaReservoir;
 
@@ -17,8 +17,9 @@ abstract class MixinTileEssentiaReservoir_UseArgInGetEssentiaType {
         method = "getEssentiaType",
         at = @At(
             value = "FIELD",
-            target = "Lnet/minecraftforge/common/util/ForgeDirection;UNKNOWN:Lnet/minecraftforge/common/util/ForgeDirection;"))
-    private ForgeDirection useFaceInstead(ForgeDirection original, @Local(argsOnly = true) ForgeDirection loc) {
+            target = "Lnet/minecraftforge/common/util/ForgeDirection;UNKNOWN:Lnet/minecraftforge/common/util/ForgeDirection;",
+            opcode = Opcodes.GETSTATIC))
+    private ForgeDirection useFaceInstead(ForgeDirection original, ForgeDirection loc) {
         return loc;
     }
 
