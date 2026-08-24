4ced076c76c0e6f41f19c1b36dd582bf38bccf98
Nana Sakura <nana-sakura@qq.com>
2026-08-24 11:49:58 +0800

    add callbackinfo import and spotless

--- src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/api/visnet/MixinTileVisNode_onRemoveThisNode.java
+++ src/main/java/dev/rndmorris/salisarcana/mixins/late/thaumcraft/api/visnet/MixinTileVisNode_onRemoveThisNode.java
@@ -6,6 +6,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 
